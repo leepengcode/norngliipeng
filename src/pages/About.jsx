7c415c5a1eacb9ca3e5fa98d92928762ab847af6
Lui Peng <norngliipeng98.info@gmail.com>
2024-06-01 09:09:31 +0700

    add text in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import profile from "../assets/1.jpg";
 const About = () => {
   return (
     <div className="mt-20 h-screen">
       <div className="w-[100%] grid grid-cols-2">
         <div className="">
-          <div className="">Image</div>
+          <div className="">Image of about</div>
         </div>
         <div className="">Text</div>
       </div>
